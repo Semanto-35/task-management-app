bbcf079d9c9482b2d04ccbc110c24696c3749b12
Semanto-35 <sabbirhossainsemanto@gmail.com>
2025-02-23 00:02:28 +0600

    Implement tailwind css , firebase authentication, routes set up in client side

---
+++ client/src/routes/Routes.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "@/pages/Home/Home";
+import Login from "@/pages/Login/Login";
+import SignUp from "@/pages/SignUp/SignUp";
+
+
+
+
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    errorElement: <div>404 Not Found</div>,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/signUp", element: <SignUp /> },
+    ],
+  },
+]);
+
+export default router;
